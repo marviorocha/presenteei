7caa5ee50fe1dd60eb36205f7ff1d44b9bb61fcf
marviorocha <marviorocha@marviorocha.com>
2021-10-04 08:57:25 -0300

    Added Steps componets

---
+++ src/lib/components/Steps.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from "$app/stores";
+  export let step = "step-primary";
+</script>
+
+<section class="p-4 mb-8">
+  <ul class="w-full steps">
+    <slot />
+  </ul>
+</section>
--- src/lib/components/Users.svelte
+++ src/lib/components/Users.svelte
@@ -1,15 +1,21 @@
 <script>
+  import Loading from "$lib/ui/Loading.svelte";
   import categories from "../../stores/Category";
 </script>
 
-<div class="grid-cols-4 grid text-white gap-4">
+<div class="grid-cols-4 grid text-white gap-2">
   {#each $categories as categories}
     <div class="flex flex-col items-center justify-center">
-      <img
-        class="w-40 h-40 border-2 border-white rounded-lg shadow"
-        src={categories.image}
-      />
-      <h3 class="text-xl p-2">{categories.name}</h3>
+      <a class="avatar" title={categories.name} href="presents/{categories.id}">
+        <div
+          class="mb-2 rounded-box w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2"
+        >
+          <img src={categories.image} alt={categories.name} />
+        </div>
+      </a>
+      <h3 class="text-xl">{categories.name}</h3>
     </div>
+  {:else}
+    <Loading />
   {/each}
 </div>
