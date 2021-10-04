9b078cccb115289ba5c15068325d105c4bee2736
marviorocha <marviorocha@marviorocha.com>
2021-10-04 08:59:08 -0300

    Added page Presents

---
+++ src/routes/presents/[id].svelte
@@ -0,0 +1,24 @@
+<script>
+  import { page } from "$app/stores";
+  import Warp from "$lib/ui/Warp.svelte";
+  import categories from "../../stores/Category";
+  import Step from "$lib/components/Steps.svelte";
+  const id = $page.params.id;
+</script>
+
+<svelte:head>
+  <title>Welcome</title>
+</svelte:head>
+
+<Warp>
+  <Step>
+    <li class="step step-primary ">Categoria</li>
+    <li class="step step-primary">Dicas</li>
+
+    <li data-content="?" class="step">Presente</li>
+    <li data-content="🎁" class="step">Finalizar</li>
+  </Step>
+  <h1 class="text-2xl font-bold">
+    Hello {id}
+  </h1>
+</Warp>
