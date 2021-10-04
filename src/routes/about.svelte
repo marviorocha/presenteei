45d4785316a26156e5f59a16198e3b524d0a73f5
marviorocha <marviorocha@marviorocha.com>
2021-10-04 08:58:30 -0300

    Added page about

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Warp from "$lib/ui/Warp.svelte";
+ 
+</script>
+
+<svelte:head>
+  <title>Index Presentei</title>
+</svelte:head>
+
+<Warp>
+  <h1 class="text-3xl">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+    similique.
+  </h1>
+</Warp>
