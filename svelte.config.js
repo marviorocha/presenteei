650a54d512b2e00f213aff17d2758dcd7d7cd989
marviorocha <marviorocha@marviorocha.com>
2021-10-20 14:40:49 -0300

    Change Adopter Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import preprocess from "svelte-preprocess";
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
+        adapter: adapter(),
         target: '#svelte',
-        adapter: netlify(),
     },
 
     preprocess: [preprocess({
